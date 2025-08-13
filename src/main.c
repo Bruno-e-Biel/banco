8710752b32ef63851202a19cea002874653229ef
Bruno Rodrigues <brunorodriguesmtv0@gmail.com>
2025-08-12 21:14:43 -0300

    Read a login.json file and print his content (Idea for data storage system)

--- src/main.c
+++ src/main.c
@@ -1,6 +1,25 @@
 #include "../include/utils.h"
+#include <stdio.h>
+#include <unistd.h>
 
 int main(void){
     welcome();
+
+    FILE *fileptr;
+    char file_buffer[255];
+
+    fileptr = fopen("data/login.json", "r");
+
+    if (fileptr == NULL){
+        printf("Error to open file.\n");
+        return -1;
+    }
+
+    while(fgets(file_buffer, 255, fileptr) != NULL){
+        printf("%s", file_buffer);
+    }
+
+    fclose(fileptr);
+
     return 0;
 }
